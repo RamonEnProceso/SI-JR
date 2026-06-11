9229853a6b66ec314090bbad457472d2b5440cc0
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-11 05:57:01 -0300

    feat: Diseñar modelo de objeto Usuario

---
+++ backend/app/models/usuario.py
@@ -0,0 +1,26 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import TIMESTAMP
+from datetime import datetime
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    rol_id: Mapped[int] = mapped_column(ForeignKey("usuario_rol.id"))
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    fecha_creacion: Mapped[datetime] = mapped_column(TIMESTAMP,server_default=func.now())
+    
+    rol: Mapped["UsuarioRol"] = relationship("UsuarioRol", back_populates="usuarios")
+    
+    def __repr__(self):
+        return f"Usuario(id={self.id}, nombre='{self.nombre}', email='{self.email}, rol='{self.rol_id}')"
+
+
