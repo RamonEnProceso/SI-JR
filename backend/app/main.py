92019f3a7376d5469be892aa7033207bda2f9984
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-11 05:59:22 -0300

    feat: Añadir router de roles al servidor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers.roles import router as roles_router
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status":"online"}
+
+app.include_router(roles_router)
