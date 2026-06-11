978de6ea3812dd6f30af2af01a4157072249627e
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-11 05:58:48 -0300

    feat: Crear router para leer roles

---
+++ backend/app/routers/roles.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db.dependencies import get_db
+from app.crud.get_usuarios_roles import get_roles
+
+router = APIRouter()
+
+@router.get("/roles")
+def get_roles_route(db: Session = Depends(get_db)):
+    
+    return get_roles(db)
