a22cfae3f9077109b6450390d250396420a84cd6
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-11 05:55:25 -0300

    feat: Inicializar SQLAlchemy, engine

---
+++ backend/app/db/engine.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql+psycopg://"
+    f"{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/"
+    f"{os.getenv('POSTGRES_DB')}"
+)
+
+engine = create_engine(DATABASE_URL)
