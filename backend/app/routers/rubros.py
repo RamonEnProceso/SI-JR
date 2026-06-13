997955f5017435f478a500fd2eed3499f199ef16
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-13 05:09:33 -0300

    fix: Corregir nombre de función get_router

--- backend/app/routers/rubros.py
+++ backend/app/routers/rubros.py
@@ -6,6 +6,5 @@ from app.crud.get_rubros import get_rubros
 router = APIRouter()
 
 @router.get("/rubros")
-def get_tipo_clientes_route(db: Session = Depends(get_db)):
-    
+def get_rubros_route(db: Session = Depends(get_db)):
     return get_rubros(db)
