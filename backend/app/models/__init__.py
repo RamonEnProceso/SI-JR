107037c7694dc9e1ea255ce58182a0d4b2001744
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-13 05:06:38 -0300

    feat: Cargar modelos en /models/__init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,8 +3,38 @@ from .usuario import Usuario
 from .usuario_rol import UsuarioRol
 from .cliente import Cliente
 from .tipo_cliente import TipoCliente
+from .foto_proceso import FotoProceso
 from .orden import Orden
 from .orden_tecnico import OrdenTecnico
 from .orden_rubro import OrdenRubro
 from .orden_estado import OrdenEstado
 from .orden_prioridad import OrdenPrioridad
+from .orden_foto import OrdenFoto
+from .orden_observacion import OrdenObservacion
+from .orden_checklist import OrdenChecklist
+from .checklist_plantilla import ChecklistPlantilla
+from .checklist_plantilla_item import ChecklistPlantillaItem
+from .orden_checklist_items import OrdenChecklistItem
+from .checklist_estado import ChecklistEstado
+from .checklist_prioridad import ChecklistPrioridad
+
+__all__ = [
+    "Usuario",
+    "UsuarioRol",
+    "Cliente",
+    "TipoCliente",
+    "FotoProceso",
+    "Orden",
+    "OrdenTecnico",
+    "OrdenRubro",
+    "OrdenEstado",
+    "OrdenPrioridad",
+    "OrdenChecklist",
+    "OrdenChecklistItem",
+    "OrdenFoto",
+    "OrdenObservacion",
+    "ChecklistPlantilla",
+    "ChecklistPlantillaItem",
+    "ChecklistEstado",
+    "ChecklistPrioridad"
+]
