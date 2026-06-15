a0d0a41f33816509034e86678d3928e188dfcb35
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-15 19:24:07 -0300

    feat: Añadir router Usuarios a la aplicación

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from app.routers.tipos_cliente import router as tipos_cliente_router
 from app.routers.rubros import router as rubros_router
 from app.routers.clientes import router as clientes_router
 from app.routers.ordenes import router as ordenes_router
+from app.routers.usuarios import router as usuarios_router
 from app import models
 
 app = FastAPI()
@@ -17,3 +18,4 @@ app.include_router(tipos_cliente_router)
 app.include_router(rubros_router)
 app.include_router(clientes_router)
 app.include_router(ordenes_router)
+app.include_router(usuarios_router)
