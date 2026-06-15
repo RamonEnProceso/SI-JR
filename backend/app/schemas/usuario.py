f7f9472a35dcb4a68deea28eb042757565cfb040
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-15 16:08:04 -0300

    feat: Diseñar schema Pydantic de Usuario

---
+++ backend/app/schemas/usuario.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+class UsuarioCreate(BaseModel):
+    nombre: str
+    email: str
+    rol_id: int = 2
+    password: str
+    
+class UsuarioUpdate(BaseModel):
+    nombre: str | None = None
+    email: str | None = None
+    rol_id: int | None = None
+    password: str | None = None
+    
+class UsuarioResponse(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+    
+    id: int
+    nombre: str
+    email: str
+    rol_id: int
+    fecha_creacion: datetime
