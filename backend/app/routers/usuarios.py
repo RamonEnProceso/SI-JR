6db11707ff5c8cd486334acaabe7f4307812c3a6
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-15 19:21:42 -0300

    feat: Añadir routers para CRUD de Usuarios

---
+++ backend/app/routers/usuarios.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.dependencies import get_db
+from app.crud.usuarios import get_usuario, get_usuarios, create_usuario, delete_usuario, update_usuario
+from app.schemas.usuario import UsuarioCreate, UsuarioResponse, UsuarioUpdate
+
+router = APIRouter(
+    prefix="/usuarios",
+    tags=["Usuarios"]
+)
+
+@router.get("/", response_model=list[UsuarioResponse])
+def get_usuarios_route(db: Session = Depends(get_db)):
+    return get_usuarios(db)
+
+@router.get("/{usuario_id}", response_model=UsuarioResponse)
+def get_usuario_route(
+    usuario_id : int,
+    db: Session = Depends(get_db)
+    ):
+    
+    usuario = get_usuario(db, usuario_id)
+    
+    if usuario is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    return usuario
+
+@router.post("/", response_model=UsuarioResponse)
+def create_usuario_route(
+    usuario_load:UsuarioCreate,
+    db: Session = Depends(get_db)
+    ):
+    return create_usuario(db, usuario_load)
+
+@router.put("/{usuario_id}", response_model=UsuarioResponse)
+def update_usuario_route(
+    usuario_id : int,
+    usuario_load: UsuarioUpdate,
+    db: Session = Depends(get_db)
+    ):
+    
+    usuario = update_usuario(db, usuario_load ,usuario_id)
+    
+    if usuario is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    return usuario
+
+@router.delete("/{usuario_id}")
+def delete_usuario_route(
+    usuario_id : int,
+    db: Session = Depends(get_db)
+    ):
+    usuario = delete_usuario(db, usuario_id)
+    
+    if usuario is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+        )
+    
+    return {"message": f"Usuario {usuario_id} eliminado"}
