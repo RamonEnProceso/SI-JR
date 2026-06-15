55990019a2466953ee413d3b45b7d44a33e2bf23
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-06-15 18:39:23 -0300

    feat: Agregar router de Ordenes a la aplicación

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from app.routers.roles import router as roles_router
 from app.routers.tipos_cliente import router as tipos_cliente_router
 from app.routers.rubros import router as rubros_router
 from app.routers.clientes import router as clientes_router
+from app.routers.ordenes import router as ordenes_router
 from app import models
 
 app = FastAPI()
@@ -15,3 +16,4 @@ app.include_router(roles_router)
 app.include_router(tipos_cliente_router)
 app.include_router(rubros_router)
 app.include_router(clientes_router)
+app.include_router(ordenes_router)
